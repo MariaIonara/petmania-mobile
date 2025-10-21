61e13f785d7cbd6ba33b1a8fb1978892867fa2a3
Ionara Porto <ionara.porto@academico.ifpb.edu.br>
2025-10-21 10:38:35 -0300

    adicionar imagem

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,23 @@
 import { Stack } from "expo-router";
+import { Image } from "react-native";
+
+function LogoTitle() {
+    return (
+      <Image source={require('../assets/images/topo.png')} />
+    );
+  }
 
 export default function RootLayout() {
-  return <Stack />;
+
+  return (
+      <Stack
+        screenOptions={{
+          headerBackImageSource: require('../assets/images/topo.png'),
+          //headerBackTitleVisible: false,
+        }}
+
+      >
+          
+      </Stack>
+  );
 }
--- app/index.jsx
+++ app/index.jsx
@@ -3,22 +3,38 @@ import { FlatList, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function Index() {
 
+  const [filteredPets, setFilteredPets] = useState([]);
   const [pets, setPets] = useState([]);
-  const [pesquisa, setPesquisa] = useState([]);
+  const [pesquisa, setPesquisa] = useState("");
 
   useEffect(() => {
     fetchPets();
   }, []);
+
+  useEffect(() => {
+    filterPets();
+  }, [pesquisa, pets]);
 
   const fetchPets = async () => {
     try {
       const response = await fetch('https://00fvcqqh-3000.brs.devtunnels.ms/api/pets');
       const data = await response.json();
       setPets(data);
+      setFilteredPets(data);
     } catch (error) {
       console.error(error);
     }
+  }
 
+  const filterPets = () => {
+    if(pesquisa.trim() == "") {
+      setFilteredPets(pets);
+    }else{
+      const resultado = pets.filter((pet) =>
+        pet.nomedopet.toLowerCase().includes(pesquisa.toLowerCase())
+      );
+      setFilteredPets(resultado);
+    }
   }
 
   const Item = ({ pet }) => (
@@ -28,24 +44,27 @@ export default function Index() {
   );
 
   return (
-    <View
-      style={styles.container}
-    >
-      <TextInput style={styles.inputs} value={pesquisa} onChangeText={setPesquisa} />
+      <View
+        style={styles.container}
+      >
+        <TextInput style={styles.inputs} value={pesquisa} onChangeText={setPesquisa} />
 
-      (pets.length) ?
-        <FlatList
-          data={pets}
-          renderItem={({ item }) => <Item pet={item} />}
-          ItemSeparatorComponent={() => <View style={{ height: 2 }} />}
-          style={{ width: "100%" }}
-        />
-      : <View>
-        <Text>Carregando</Text>
+        {pets.length > 0 ? (
+          <FlatList
+            data={filteredPets}
+            keyExtractor={(item) => item.id?.toString()}
+            renderItem={({ item }) => <Item pet={item} />}
+            ItemSeparatorComponent={() => <View style={{ height: 2 }} />}
+            style={{ width: "100%" }}
+          />
+          ) : ( 
+          <View>
+            <Text>Carregando</Text>
+          </View>
+        )
+      }
+        
       </View>
-
-      
-    </View>
   );
 }
 
